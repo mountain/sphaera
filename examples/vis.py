58c7a1ca9a5f35cf08aadab74b8475659d1c357f
Mingli Yuan <mingli.yuan@gmail.com>
2023-12-10 04:19:07 +0800

    add vis

---
+++ examples/vis.py
@@ -0,0 +1,13 @@
+import torch as th
+
+from sphaera.plot.plot3d import plot_scalar
+
+
+def strip(data):
+    d = data.reshape(1, 1, 721, 1504, 1)
+    return d[:, :, :, 32:1472, 0:1]
+
+
+with th.no_grad():
+    spectrum = th.load('examples/spectrum.dat')
+    plot_scalar('spectrum', strip(spectrum))
