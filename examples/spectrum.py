5cd08e0f93f2fd79e967eed19bafe4e3ee979c20
Mingli Yuan <mingli.yuan@gmail.com>
2023-12-18 17:21:39 +0800

    align vectors

--- examples/spectrum.py
+++ examples/spectrum.py
@@ -64,6 +64,6 @@ for ix in range(1504):
         frame = cross(axis, perp)
         signature = th.sign(dot(r, axis))
         frame = scale(frame, signature)
-        spectrum[0:1, 0:1, jx, ix, 0:1] = th.sum(dot(frame, wnd) * sph.dVol) / th.sum(sph.dVol)
+        spectrum[0:1, 0:1, jx, ix, 0:1] = th.sum(dot(frame, wnd) * sph.dV) / th.sum(sph.dV)
 
 th.save(spectrum, './examples/spectrum.dat')
