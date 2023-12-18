6ba185f62ce78b79c2e580297834d971391be61d
Mingli Yuan <mingli.yuan@gmail.com>
2023-12-18 16:03:23 +0800

    align vectors

--- examples/finder.py
+++ examples/finder.py
@@ -79,7 +79,7 @@ class BestVeloFinder(L.LightningModule):
 
     def forward(self, x):
         ix, jx = x
-        axis = r_0[:, :, jx, ix, 0:1], r_1[:, :, jx, ix, 0:1], r_2[:, :, jx, ix, 0:1]
+        axis = sph.align(r_0[:, :, jx, ix, 0:1]), sph.align(r_1[:, :, jx, ix, 0:1]), sph.align(r_2[:, :, jx, ix, 0:1])
         signature = sph.align(th.sign(dot(r, axis)))
         scale = signature * self.base_velo[:, :, jx, ix, 0:1]
         frame = cross(r, axis)
