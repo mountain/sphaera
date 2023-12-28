008320e4ffabfe9a9954adc45ae1d55412a1fc0b
Mingli Yuan <mingli.yuan@gmail.com>
2023-12-29 01:50:48 +0800

    add visualization examples

--- examples/finder.py
+++ examples/finder.py
@@ -98,7 +98,7 @@ class BestFinder(L.LightningModule):
         ix = th.fmod(8 + lng + alpha * 180 / th.pi, 360).long()
         jx = (eta * 180 / th.pi).long()
 
-        return u0, v0, a0 + (th.cos(theta) + a0 * th.sin(theta)) * ds, self.a[:, :, jx, ix, 0:1]
+        return u0, v0, a0 + (th.cos(theta) + a0 * th.sin(theta)) * ds, (self.a[:, :, jx, ix, 0:1]).to(th.device('mps'))
 
     def training_step(self, batch, batch_idx):
         paths = batch
