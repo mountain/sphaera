51b39c80420b5d6e584e3dea31d7915731f1e6a4
Mingli Yuan <mingli.yuan@gmail.com>
2023-12-29 10:16:49 +0800

    add visualization examples

--- examples/vis_finder.py
+++ examples/vis_finder.py
@@ -1,6 +1,8 @@
 import torch as th
+import sphaera as sph
 
 from sphaera.plot.plot3d import plot_scalar
+from sphaera.core3d.gridsys.regular3 import RegularGrid
 
 
 def strip(data):
@@ -9,5 +11,27 @@ def strip(data):
 
 
 with th.no_grad():
+    # --------------------------------
+    # Step 1: Setup grid system
+    # --------------------------------
+
+    sph.bind(RegularGrid(
+        basis='lng,lat,alt',
+        W=1504, L=721, H=2,
+        east=0 - 0.25 * 32, west=360 + 0.25 * 32,
+        north=-89.999, south=89.999,
+        upper=0.99, lower=1.01
+    ))
+    sph.use('x,y,z')
+    sph.use('xyz')
+    sph.use('theta,phi,r')
+    sph.use('thetaphir')
+
     a = th.load('examples/a.dat')
+    a = th.reshape(a, [1, 1, 721, 1504, -1])
+    a = th.concatenate([a, a], dim=-1).double()
+
     plot_scalar('a', strip(a))
+
+    b = sph.laplacian(a)
+    plot_scalar('b', strip(b))
